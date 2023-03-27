737c51c6f86f8fd92853c7b83c88ba38d9f66116
Phong <phamkhacthanhphong@gmail.con>
2023-03-27 21:09:31 +0700

    second commit

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ mongoose
     .then(() => {
         console.log("Connected to DB");
     })
-    .catch((err) => {
-        console.log("err", err);
+    .catch((error) => {
+        console.log("error", error);
     })
 
 
@@ -41,20 +41,43 @@ app.post('/createProduct', (req, res) => {
     product.save()
     res.redirect('/')
 })
-app.get('/editProduct', async (req, res) => {
+
+app.get('/editProduct?:id', async (req, res) => {
     const id = req.query.id
-    const product = await productModel.findById(id)
+    var product = await productModel.findById(id)
     res.render('edit-product', {'product': product})
 })
+app.post('/editProduct?:id', async (req, res) => {
+    const id = req.body.id
+    const name = req.body.txtName
+    const picture = req.body.picture
+    const price = req.body.price
+    const description = req.body.description
+
+    try{
+        const product = await productModel.findById(id)
+        product.name = name
+        product.picture = picture
+        product.price = price
+        product.description = description
+        product.save()
+        console.log("update successfully")
+        res.redirect('/')
+    } catch (error){
+        console.log("somethings went wrong")
+        console.log(error)
+    }
+})
+
 //product detail (1)
-app.get('/viewProduct/:id', async(req, res) => {
+app.get('/viewProduct?:id', async(req, res) => {
     const id = req.query.id
     const product = await productModel.findById(id)
     res.render('view-product', {'product': product})
 })
-app.get('/deleteProduct', async (req, res) => {
+app.get('/deleteProduct?:id', async (req, res) => {
     const id = req.query.id
-    const product = await productModel.deleteOne(id)
+    await productModel.deleteOne({'_id' : id})
     res.redirect('/')
 })
 
